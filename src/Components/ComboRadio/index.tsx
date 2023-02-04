3bb2b7e48a4492854be59cfeab5f0b299c88a1e9
e-maks <matantsev0@gmail.com>
2023-02-05 00:01:52 +0300

    Disabel radio after reset

--- src/Components/ComboRadio/index.tsx
+++ src/Components/ComboRadio/index.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect, useState} from "react";
+import React, {useCallback} from "react";
 import {Button} from "@mui/material";
 import {selectedItems} from "../../constants/typings";
 
