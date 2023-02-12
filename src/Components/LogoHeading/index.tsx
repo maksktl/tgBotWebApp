f6ac549077c28b6b5703a3915f252b45d04a2294
e-maks <matantsev0@gmail.com>
2023-02-12 16:12:01 +0300

    Fix header

--- src/Components/LogoHeading/index.tsx
+++ src/Components/LogoHeading/index.tsx
@@ -5,7 +5,7 @@ export const LogoHeading: React.FC = () => {
     return (
         <Box className="LogoHeader">
             <img className="Logo" alt="logo" />
-            <h1 >АВТОМАТИЗАЦИЯ ПОИСКА АКТУАЛЬНЫХ СВЯЗОК</h1>
+            <h1 className="Header">АКТУАЛЬНЫЕ СВЯЗКИ ДЛЯ АРБИТРАЖА НА P2P ПЛАТФОРМАХ</h1>
         </Box>
     )
 };
